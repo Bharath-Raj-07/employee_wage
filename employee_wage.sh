94771af243edc5268e2ff803c17aff32101ddb0a
Bharath-Raj-07 <bharathrajlifeline@gmail.com>
2024-01-13 12:53:24 +0530

    Monthly Wage

--- employee_wage.sh
+++ employee_wage.sh
@@ -1,12 +1,5 @@
-echo "Enter 1 for present and 0 for absent"
-read var
-case $var in
-1)
-echo "Present" ;;
-0)
-echo "Absent" ;;
-*)
-echo "Invalid entry"
-;;
-esac
-
+days=20
+hours=8
+wphour=20
+mwage=$((($days*$hours)*wphour))
+echo "$mwage"
